c9d5611d7cd8db394b4ff03bd59dc0a6061e3215
Wiraswanti Rismanda Putri <wiraswantirismandaputri@gmail.com>
2022-04-04 16:58:24 +0700

    minggu7

--- fileUpload.php
+++ fileUpload.php
@@ -4,7 +4,7 @@
     $target_path =$target_path . basename(
     $_FILES['uploadedfile']['name']);
 
-    if(move_uploaded_file($_FILES['uploadedfile']['tmp_name'],$target_path )){
+    if(move_uploaded_file($_FILES['uploadedfile']['tmp_name'],$target_path)){
         echo "The file " . basename($_FILES['uploadedfile']['name'])."has been uploaded";
     }else{
         echo "There was an error uploading the file, please try again!";
